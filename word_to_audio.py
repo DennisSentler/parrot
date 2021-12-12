15be870833689565de76862115633bbeabab135c
Dennis Sentler <dennis.sent.93@gmail.com>
2021-12-13 00:52:12 +0100

    use all gcp voices, script language parameter

--- word_to_audio.py
+++ word_to_audio.py
@@ -42,25 +42,17 @@ def watson_cloud_tts(text, output_path, username, password):
         print('audios in voice ' + voice + ' are created')
 
 
-def google_cloud_tts(text, output_path):
-    voice_list = [
-        ('de-DE', 'de-DE-Standard-A'), 
-        ('de-DE', 'de-DE-Standard-B'),
-        ('de-DE', 'de-DE-Standard-C'), 
-        ('de-DE', 'de-DE-Standard-D'),
-        ('de-DE', 'de-DE-Standard-E'),
-        ('de-DE', 'de-DE-Standard-F'),
-        ('de-DE', 'de-DE-Wavenet-A'), 
-        ('de-DE', 'de-DE-Wavenet-B'),
-        ('de-DE', 'de-DE-Wavenet-C'), 
-        ('de-DE', 'de-DE-Wavenet-D'),
-        ('de-DE', 'de-DE-Wavenet-E'), 
-        ('de-DE', 'de-DE-Wavenet-F')]
+def google_cloud_tts(text, lang_list, output_path):
         
     client = texttospeech.TextToSpeechClient()
+    # pull all voices from gcloud
+    voice_list = client.list_voices()
     input_text = texttospeech.SynthesisInput(text=text)
     # Text to speech api request
-    for language, voice in voice_list:
+    for voice in voice_list.voices:
+        if voice.language_codes[0] not in lang_list:
+            continue
+
         # Generate random voice characteristics
         random_rate = 1
         random_volume = 0
@@ -68,8 +60,8 @@ def google_cloud_tts(text, output_path):
 
         for i in range(10):
             voice_config = texttospeech.VoiceSelectionParams(
-                language_code=language,
-                name=voice)
+                language_code=voice.language_codes[0],
+                name=voice.name)
             audio_config = texttospeech.AudioConfig(
                 audio_encoding=texttospeech.AudioEncoding.LINEAR16,
                 speaking_rate=random_rate,
@@ -77,8 +69,9 @@ def google_cloud_tts(text, output_path):
                 volume_gain_db=random_volume)
 
             response = client.synthesize_speech(input=input_text, voice=voice_config, audio_config=audio_config)
-
-            file_name = 'gc_{0}{1}%{2}db{3}st.wav'.format(voice, random_rate, random_volume, random_pitch)
+            
+            gender_char = voice.ssml_gender.name[0].lower()
+            file_name = '{4}_gcp_{0}_{5}_{1}r{2}v{3}p.wav'.format(voice.name, random_rate, random_volume, random_pitch, text, gender_char)
             with open(output_path + '/' + file_name, 'wb') as f:
                 f.write(response.audio_content)
 
@@ -86,7 +79,7 @@ def google_cloud_tts(text, output_path):
             random_volume = randint(-5, 15)/10
             random_pitch = randint(-50, 50)/10
 
-        print('audios in voice {0} are created'.format(voice))
+        print('audios in voice {0} are created'.format(voice.name))
 
 
 def microsoft_tts(text, output_path, token):
@@ -139,9 +132,11 @@ def main():
     #parser.add_argument('watson_username', help='IBM Watson TTS subscription username')
     #parser.add_argument('watson_password', help='IBM Watson TTS subscription password')
     parser.add_argument('--words', nargs='*', help='Words to be pronounced', default=['albert einstein'])
+    parser.add_argument('--lang', nargs='*', help='To generate for languages', default=['en-US'])
     args = parser.parse_args()
 
     word_list = args.words
+    lang_list =args.lang
 
     # Authentication for Microsoft Speech Service
     # Get an authorization token
@@ -158,7 +153,7 @@ def main():
             print(path + ' directory is created!')
 
         #microsoft_tts(word, path, ms_token_response.text)
-        google_cloud_tts(word, path)
+        google_cloud_tts(word, lang_list, path)
         #watson_cloud_tts(word, path, args.watson_username, args.watson_password)
 
         print('all audios for ' + word + ' are generated!')
