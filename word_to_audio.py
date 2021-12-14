2ed0ca7eea35a062c00edb42c99176d8f4f16a9d
Dennis Sentler <dennis.sent.93@gmail.com>
2021-12-14 20:12:10 +0100

    updated watson sdk usage

--- word_to_audio.py
+++ word_to_audio.py
@@ -1,49 +1,56 @@
 import argparse
 import os
-import requests
-import sys
-
 from random import randint
+
+import requests
 from google.cloud import texttospeech
-from watson_developer_cloud import TextToSpeechV1
+from ibm_watson import TextToSpeechV1
 
 
-def watson_cloud_tts(text, output_path, username, password):
-    voice_list = ['en-US_AllisonVoice', 'en-US_LisaVoice', 'en-US_MichaelVoice', 'en-GB_KateVoice']
-
-    text_to_speech = TextToSpeechV1(
-        url='https://stream.watsonplatform.net/text-to-speech/api',
-        username=username,
-        password=password)
+def watson_cloud_tts(text, lang_list, output_path):
+    # authentification with IBM_CREDENTIALS_FILE
+    text_to_speech = TextToSpeechV1()
+    # get voice list from IBM, watson
+    voice_list = text_to_speech.list_voices().get_result()["voices"]
 
     # Text to speech api request
     for voice in voice_list:
+        # skip not requested voice languages
+        if voice["language"] not in lang_list:
+            continue
+
         # Generate random voice characteristics
-        random_rate = 'default'
-        random_volume = 'default'
-        random_pitch = 'default'
+        random_rate = "default"
+        random_pitch = "default"
 
         for i in range(10):
-            ssml = '<prosody rate="{0}" volume="{1}" pitch="{2}"> {3} </prosody>'.format(random_rate, \
-                random_volume, random_pitch, text)
-            response = text_to_speech.synthesize(
-                ssml,
-                accept='audio/wav',
-                voice=voice)
+            ssml = (
+                '''
+                <prosody rate="{0}" pitch="{1}">
+                    {2}
+                </prosody>
+                '''.format(
+                    random_rate, random_pitch, text
+                )
+            )
+            response = text_to_speech.synthesize(ssml, accept="audio/wav",
+                                                 voice=voice["name"])
 
-            file_name = 'watson_{0}{1}{2}{3}.wav'.format(voice, random_rate, random_volume, random_pitch)
-            with open(output_path + '/' + file_name, 'wb') as f:
-                f.write(response.content)
+            file_name = "{3}_watson_{0}_{4}_{1}{2}.wav".format(
+                voice["name"], random_rate,
+                random_pitch, text, voice["gender"][0]
+            )
+            with open(output_path + "/" + file_name, "wb") as f:
+                f.write(response.get_result().content)
 
-            random_rate = str(randint(-15, 15)) + '%'
-            random_volume = str(randint(75, 100))
-            random_pitch = str(randint(-10, 10)) + 'Hz'
+            random_rate = str(randint(-15, 15)) + "%"
+            random_pitch = str(randint(-10, 10)) + "Hz"
 
-        print('audios in voice ' + voice + ' are created')
+        print("audios in voice " + voice["name"] + " are created")
 
 
 def google_cloud_tts(text, lang_list, output_path):
-        
+    # authentification from  env var GOOGLE_APPLICATION_CREDENTIALS
     client = texttospeech.TextToSpeechClient()
     # pull all voices from gcloud
     voice_list = client.list_voices()
@@ -60,104 +67,135 @@ def google_cloud_tts(text, lang_list, output_path):
 
         for i in range(10):
             voice_config = texttospeech.VoiceSelectionParams(
-                language_code=voice.language_codes[0],
-                name=voice.name)
+                language_code=voice.language_codes[0], name=voice.name
+            )
             audio_config = texttospeech.AudioConfig(
                 audio_encoding=texttospeech.AudioEncoding.LINEAR16,
                 speaking_rate=random_rate,
                 pitch=random_pitch,
-                volume_gain_db=random_volume)
+                volume_gain_db=random_volume,
+            )
 
-            response = client.synthesize_speech(input=input_text, voice=voice_config, audio_config=audio_config)
-            
+            response = client.synthesize_speech(
+                input=input_text, voice=voice_config, audio_config=audio_config
+            )
+
             gender_char = voice.ssml_gender.name[0].lower()
-            file_name = '{4}_gcp_{0}_{5}_{1}r{2}v{3}p.wav'.format(voice.name, random_rate, random_volume, random_pitch, text, gender_char)
-            with open(output_path + '/' + file_name, 'wb') as f:
+            file_name = "{4}_gcp_{0}_{5}_{1}r{2}v{3}p.wav".format(
+                voice.name, random_rate, random_volume,
+                random_pitch, text, gender_char
+            )
+            with open(output_path + "/" + file_name, "wb") as f:
                 f.write(response.audio_content)
 
-            random_rate = randint(75, 125)/100
-            random_volume = randint(-5, 15)/10
-            random_pitch = randint(-50, 50)/10
+            random_rate = randint(75, 125) / 100
+            random_volume = randint(-5, 15) / 10
+            random_pitch = randint(-50, 50) / 10
 
-        print('audios in voice {0} are created'.format(voice.name))
+        print("audios in voice {0} are created".format(voice.name))
 
 
 def microsoft_tts(text, output_path, token):
-    voice_list = [('en-AU', 'en-AU, Catherine'), ('en-AU', 'en-AU, HayleyRUS'), 
-    ('en-CA', 'en-CA, Linda'), ('en-CA', 'en-CA, HeatherRUS'),
-    ('en-GB', 'en-GB, Susan, Apollo'), ('en-GB', 'en-GB, HazelRUS'),
-    ('en-GB', 'en-GB, George, Apollo'), ('en-IE', 'en-IE, Sean'),
-    ('en-IN', 'en-IN, Heera, Apollo'), ('en-IN', 'en-IN, PriyaRUS'),
-    ('en-IN', 'en-IN, Ravi, Apollo'), ('en-US', 'en-US, ZiraRUS'),
-    ('en-US', 'en-US, JessaRUS'), ('en-US', 'en-US, BenjaminRUS')]
+    voice_list = [
+        ("en-AU", "en-AU, Catherine"),
+        ("en-AU", "en-AU, HayleyRUS"),
+        ("en-CA", "en-CA, Linda"),
+        ("en-CA", "en-CA, HeatherRUS"),
+        ("en-GB", "en-GB, Susan, Apollo"),
+        ("en-GB", "en-GB, HazelRUS"),
+        ("en-GB", "en-GB, George, Apollo"),
+        ("en-IE", "en-IE, Sean"),
+        ("en-IN", "en-IN, Heera, Apollo"),
+        ("en-IN", "en-IN, PriyaRUS"),
+        ("en-IN", "en-IN, Ravi, Apollo"),
+        ("en-US", "en-US, ZiraRUS"),
+        ("en-US", "en-US, JessaRUS"),
+        ("en-US", "en-US, BenjaminRUS"),
+    ]
 
+    tts_url = "https://westus.tts.speech.microsoft.com/cognitiveservices/v1"
     # Authentication
     # Text to speech api request
     for language, voice in voice_list:
         # Generate random voice characteristics
-        random_rate = str(0) + '%'
-        random_volume = str(0) + '%'
-        random_pitch = str(0) + 'Hz'
+        random_rate = str(0) + "%"
+        random_volume = str(0) + "%"
+        random_pitch = str(0) + "Hz"
 
         for i in range(10):
-            tts_url = 'https://westus.tts.speech.microsoft.com/cognitiveservices/v1'
-            tts_header = {'X-Microsoft-OutputFormat':'riff-16khz-16bit-mono-pcm',
-            'Authorization':token,
-            'Content-Type':'application/ssml+xml'}
+            tts_header = {
+                "X-Microsoft-OutputFormat": "riff-16khz-16bit-mono-pcm",
+                "Authorization": token,
+                "Content-Type": "application/ssml+xml",
+            }
 
-            ssml = '<prosody rate="{0}" volume="{1}" pitch="{2}"> {3} </prosody>'.format(random_rate, \
-                random_volume, random_pitch, text)
-            tts_content = """<speak version='1.0'
-            xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='{0}'>
-            <voice name='Microsoft Server Speech Text to Speech Voice ({1})'> {2}
-            </voice> </speak>""".format(language, voice, ssml)
+            ssml = (
+                '''
+                <prosody rate="{0}" volume="{1}" pitch="{2}">
+                    {3}
+                </prosody>
+                '''.format(
+                    random_rate, random_volume, random_pitch, text
+                )
+            )
+            tts_content = '''
+            <speak version='1.0'
+                xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='{0}'>
+                <voice
+                    name='Microsoft Server Speech Text to Speech Voice ({1})'>
+                    {2}
+                </voice>
+            </speak>'''.format(
+                language, voice, ssml
+            )
 
-            tts_response = requests.post(tts_url, headers=tts_header, data=tts_content)
+            tts_response = requests.post(
+                tts_url, headers=tts_header, data=tts_content)
 
-            file_name = 'ms_{0}{1}{2}{3}.wav'.format(voice.replace(' ', ''), random_rate, random_volume, random_pitch)
-            with open(output_path + '/' + file_name, 'wb') as f:
+            file_name = "ms_{0}{1}{2}{3}.wav".format(
+                voice.replace(
+                    " ", ""), random_rate, random_volume, random_pitch
+            )
+            with open(output_path + "/" + file_name, "wb") as f:
                 f.write(tts_response.content)
 
-            random_rate = str(randint(-20, 20)) + '%'
-            random_volume = str(randint(-20, 20)) + '%'
-            random_pitch = str(randint(-15, 15)) + 'Hz'
+            random_rate = str(randint(-20, 20)) + "%"
+            random_volume = str(randint(-20, 20)) + "%"
+            random_pitch = str(randint(-15, 15)) + "Hz"
 
-        print('audios in voice ' + voice + ' are created')
+        print("audios in voice " + voice + " are created")
 
 
 def main():
-    parser = argparse.ArgumentParser(description="create a directory for each word, \
-        and 320 different voices per word in its directory")
-    #parser.add_argument('ms_subscription_key', help='Microsoft cognitive service subscription key')
-    #parser.add_argument('watson_username', help='IBM Watson TTS subscription username')
-    #parser.add_argument('watson_password', help='IBM Watson TTS subscription password')
-    parser.add_argument('--words', nargs='*', help='Words to be pronounced', default=['albert einstein'])
-    parser.add_argument('--lang', nargs='*', help='To generate for languages', default=['en-US'])
+    parser = argparse.ArgumentParser(
+        description="create a directory for each word"
+    )
+    parser.add_argument(
+        "--words", nargs="*", help="Words to be pronounced",
+        default=["albert einstein"]
+    )
+    parser.add_argument(
+        "--lang", nargs="*", help="To generate for languages",
+        default=["en-US"]
+    )
     args = parser.parse_args()
 
     word_list = args.words
-    lang_list =args.lang
-
-    # Authentication for Microsoft Speech Service
-    # Get an authorization token
-    # TODO: get a new authentication token per nine minutes
-    #ms_token_url = 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken'
-    #ms_token_header = {'Ocp-Apim-Subscription-Key':args.ms_subscription_key}
-    #ms_token_response = requests.post(ms_token_url, headers = ms_token_header)
+    lang_list = args.lang
 
     for word in word_list:
         # Create an individual directory for each keyword if not existed
-        path = 'generated_words/' + word.replace(' ', '')
+        path = "generated_words/" + word.replace(" ", "")
         if not os.path.exists(path):
             os.makedirs(path)
-            print(path + ' directory is created!')
+            print(path + " directory is created!")
 
-        #microsoft_tts(word, path, ms_token_response.text)
+        # microsoft_tts(word, path, ms_token_response.text)
         google_cloud_tts(word, lang_list, path)
-        #watson_cloud_tts(word, path, args.watson_username, args.watson_password)
+        watson_cloud_tts(word, lang_list, path)
 
-        print('all audios for ' + word + ' are generated!')
+        print("all audios for '" + word + "' are generated!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
